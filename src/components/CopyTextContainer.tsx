0f2603536e3e5681c5128cc8c35c396555494429
David <77506410+gdavidev@users.noreply.github.com>
2025-01-26 21:29:46 -0300

    (fix): fixed responsiveness on the overlay buttons

--- src/components/CopyTextContainer.tsx
+++ src/components/CopyTextContainer.tsx
@@ -19,20 +19,23 @@ export default function CopyTextContainer(props: CopyTextContainerProps) {
     return (
         <div
             onClick={copyToClipboard}
-            className={'group select-none w-fit overflow-hidden h-12 cursor-pointer ' + (props.className ?? '')}>
+            className={'group select-none w-fit overflow-hidden h-11 md:h-12 cursor-pointer ' + (props.className ?? '')}>
             <div
                 onMouseOver={() => setCurrentHoverAnimation('animate-smooth-pan')}
                 onMouseOut={() => setCurrentHoverAnimation('animate-smooth-pan-reverse')}
                 className={'-mx-1 ' + currentHoverAnimation}>
                 <div
                     onMouseOver={() => setCopied(false)}
-                    className='px-4 pb-2 pt-4 -mt-2 bg-card-background text-primary-red text-end text-2xl group-hover:-mt-14 duration-200'>
-                    <div className='flex justify-between items-center gap-x-16'>
+                    className={
+                        'px-4 pb-2 pt-4 -mt-2 bg-card-background text-primary-red text-end '
+                        + 'text-lg md:text-2xl group-hover:-mt-[3.25rem] md:group-hover:-mt-14 duration-200'
+                    }>
+                    <div className='flex justify-between items-center gap-x-8 md:gap-x-16'>
                         <FontAwesomeIcon icon={['fas', 'envelope']}/>
                         {props.text}
                     </div>
                 </div>
-                <div className='size-full text-white bg-primary-red pt-2 pb-4 -mb-2 text-end text-2xl pe-4 origin-top-left'>
+                <div className='size-full text-white bg-primary-red pt-2 pb-4 -mb-2 text-end text-lg md:text-2xl pe-4 origin-top-left'>
                     {isCopied ?
                         <span className='font-bold'>Copiado!</span> :
                         <span className=''>Copiar</span>
